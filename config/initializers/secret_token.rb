1d49cda6a8dfd7a02208d314bf80f46ca0fc4bfd
aakash <aakashaggarwal.1993@gmail.com>
2014-04-27 01:35:25 +0530

    “Initial.”

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geniieblog::Application.config.secret_key_base = 'a1491673dce7c9f078970a1d80cb96b1aa0dd9b6dc3138c58aa7f021feb374e830e89f8140aebb9c5a7c66fc4c75dcc0af4050794cb98ef540d0a21787325db7'
